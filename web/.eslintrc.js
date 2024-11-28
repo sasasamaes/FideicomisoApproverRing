606ec62a2e0e7610dbe67b4e1c6e2a6894c5260d
Aroso Emmanuel  Adedeji <57766083+emarc99@users.noreply.github.com>
2024-11-28 14:27:28 +0100

    update .eslintrc.js

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,6 +1,20 @@
 module.exports = { 
   parser: '@typescript-eslint/parser', 
-  plugins: ['@typescript-eslint/eslint-plugin'], 
+  plugins: ['@typescript-eslint/eslint-plugin', 'import'],
+  parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      project: './tsconfig.json',
+      tsconfigRootDir: __dirname,
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {},
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx']
+        }
+      }
+    },
   extends: [
     'eslint:recommended', 
     'plugin:@typescript-eslint/recommended',
